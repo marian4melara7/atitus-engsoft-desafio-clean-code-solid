ab537a41dcf87f658e9da96ee162e1120b41040d
Mariana M. Melara <167313774+marian4melara7@users.noreply.github.com>
2026-04-28 21:48:02 -0300

    Optimize encontrarProdutosComuns function
    Refactor to optimize common product finding from O(n²) to O(n) using a Set.

--- 02-big-o/src/encontrarProdutosComuns.js
+++ 02-big-o/src/encontrarProdutosComuns.js
@@ -1,16 +1,6 @@
-/**
- * ❌ OTIMIZAÇÃO O(n²) para O(n)
- */
 function encontrarProdutosComuns(listaA, listaB) {
-  const comuns = [];
-  for (let i = 0; i < listaA.length; i++) {
-    for (let j = 0; j < listaB.length; j++) {
-      if (listaA[i] === listaB[j]) {
-        comuns.push(listaA[i]);
-      }
-    }
-  }
-  return comuns;
+  const setB = new Set(listaB);
+  return listaA.filter(produto => setB.has(produto));
 }
 
 module.exports = encontrarProdutosComuns;
